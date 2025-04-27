fa52cf55ab0ccad3b4259b63a000de1cf3bac970
Satyaprakash <sk9334069077@gmail.com>
2025-04-28 03:29:24 +0530

    Simple interest

--- src/datatype/DataTypes.java
+++ src/datatype/DataTypes.java
@@ -33,18 +33,44 @@ public class DataTypes {
 //        System.out.println("Your division is:- " +div);
 //
 
+//        Scanner sc=new Scanner(System.in);
+//
+//        System.out.println("Enter first num:");
+//        int a= sc.nextInt();
+//
+//
+//        System.out.println("Enter second num:");
+//        int b= sc.nextInt();
+//        int sum= a+b;
+//        System.out.println("Sum:- "+sum);
+//
+
+
+
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter first num:");
-        int a= sc.nextInt();
+        System.out.println("Enter principle:-");
+        Float p= sc.nextFloat();
 
 
-        System.out.println("Enter second num:");
-        int b= sc.nextInt();
-        int sum= a+b;
-        System.out.println("Sum:- "+sum);
+        System.out.println("Enter rate of interest:-");
+        Float r= sc.nextFloat();
 
-        
+        System.out.println("Enter time:-");
+        Float t= sc.nextFloat();
+
+        Float si=(p*r*t)/100;
+
+        System.out.println("Principle:- "+p);
+        System.out.println("Rate:- "+r);
+        System.out.println("Time:- "+t);
+
+        System.out.println("Simple interest:- "+ si);
+
+
+
+
+
 
     }
 }
