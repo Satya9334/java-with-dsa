259b2a8ee3b80a714bb95e8402bfb093a0433f3e
Satyaprakash <sk9334069077@gmail.com>
2025-05-03 02:50:51 +0530

    operators

---
+++ src/Operatorsss.java
@@ -0,0 +1,77 @@
+public class Operatorsss {
+    public static void main(String[] args) {
+
+
+      ///////// Arithemeic operators//////////////////
+
+//        int p=20,q=10;
+//        System.out.println(p+q); //Addition
+//        System.out.println(p-q); //subtraction
+//        System.out.println(p*q); //multiplication
+//        System.out.println(p/q); //division
+//        System.out.println(p%q); //module
+
+            /////// //Relational operators/////////////
+//        int i=5,j=7;
+//        System.out.println(i==j); //is Equal to
+//        System.out.println(i!=j); //Not Equal to
+//        System.out.println(i>j);  //Greater than
+//        System.out.println(i<j);  //Less than
+//        System.out.println(i>=j); //Greater than Equal
+//        System.out.println(i<=j); //Less than Equal
+//
+
+        /// //////////logical operators/////////
+
+        /// Logical AND operators
+//         int a=15, b=13,c=9;
+//        System.out.println((a>b) && (a>c)); // Logical AND operators
+
+       //// Logical OR operators
+//        int p=10, q=8,r=15;
+//        System.out.println((p>q)||(p>r)); // True
+
+        ////// Not operators
+//        int x=50,y=50,z=70;
+//        System.out.println(!(x==y));  //false
+//        System.out.println(!(y==z));  //True
+
+
+        ///Assignment operators
+
+        int p =10;
+        int q;
+
+        ///=
+        q = p;
+        System.out.println(q); // 10
+
+        /// +=
+        p+=q; //p=p+q;  // p = 10+10 =20;
+        System.out.println(p);   // 20
+
+        /// -=
+        p-=q; //p=p-q;  p=10-10;
+        System.out.println(p);  //20 -10 =10
+
+        /// *=
+        p*=q; // p=p*q; // p=10*10 = 100
+        System.out.println(p);
+
+
+        /// /=
+        p/=q; // p=p/q // p=10 /10
+        System.out.println(p);
+
+          /// %=
+        p%=q; // p=p%q //p=10%10
+        System.out.println(p);
+
+
+
+
+
+
+
+    }
+}
