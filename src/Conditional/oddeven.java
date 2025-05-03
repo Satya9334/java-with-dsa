0f8472ab8c39f31eb4a95bcae0f696d6a25b772c
Satyaprakash <sk9334069077@gmail.com>
2025-05-04 02:05:44 +0530

    Evenodd:- divisible by 5 or not

--- src/Conditional/oddeven.java
+++ src/Conditional/oddeven.java
@@ -6,22 +6,29 @@ public class oddeven {
     public static void main(String[] args) {
                                                                     // Question/// Take positive integer input and tell if is even and odd
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number");
+//        System.out.println("Enter a number");
+//        int n= sc.nextInt();
+//        if (n%2==0){  // n even hai
+//            System.out.println("The number you entered is Even");
+//        }
+//
+//                                                                  // if (n%2!=0){ //n odd hai ////aise bhi likh sakte hai ya else ka use karenge
+//
+//        else {
+//            System.out.println("the number you Entered is odd");
+
+
+
+/// Take positive integer input and tell if it is divisible by 5 or not
+
+
+        System.out.println("Enter a number:");
         int n= sc.nextInt();
-        if (n%2==0){  // n even hai
-            System.out.println("The number you entered is Even");
+        if (n%5==0){
+            System.out.println("Yes it is divisible by 5");
         }
-
-                                                                  // if (n%2!=0){ //n odd hai ////aise bhi likh sakte hai ya else ka use karenge
-
         else {
-            System.out.println("the number you Entered is odd");
-
-
-
-
-
-
+            System.out.println("Not divisible by 5");
         }
 
     }
