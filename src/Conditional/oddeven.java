1e148b544520fc3ff12b31265de208ec40de6e4f
Satyaprakash <sk9334069077@gmail.com>
2025-05-04 01:48:17 +0530

    Evenodd change the if condition and add the else condition

--- src/Conditional/oddeven.java
+++ src/Conditional/oddeven.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class oddeven {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter a number");
         int n= sc.nextInt();
         if (n%2==0){  // n even hai
             System.out.println("The number you entered is Even");
         }
-        if (n%2!=0){ //n odd hai
+
+       // if (n%2!=0){ //n odd hai ////aise bhi likh sakte hai ya else ka use karenge
+
+        else {
             System.out.println("the number you Entered is odd");
 
         }
