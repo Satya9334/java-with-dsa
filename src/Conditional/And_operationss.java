c31f3b76a9799ee663238d74463acf75bd11b4a0
Satyaprakash <sk9334069077@gmail.com>
2025-05-10 01:38:27 +0530

    3 digit number

---
+++ src/Conditional/And_operationss.java
@@ -0,0 +1,17 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class And_operationss {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n= sc.nextInt();
+        if (n>99 && n<1000){
+            System.out.println("This is a 3 digit number");
+        }
+        else {
+            System.out.println("It is a not 3 digit number");
+        }
+    }
+}
