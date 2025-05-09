3057d0c1d8b5d30edeac06e37946f260edf68d15
Satyaprakash <sk9334069077@gmail.com>
2025-05-10 02:53:01 +0530

    Stringsss value

--- src/datatype/Stringsss.java
+++ src/datatype/Stringsss.java
@@ -6,24 +6,21 @@ public class Stringsss {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter user name:");
+        System.out.println("Enter user name");
         String name= sc.nextLine();
-
-
-        System.out.println("Enter user age:");
-        String age= sc.nextLine();
-
-        System.out.println("Enter user address:");
-        String address= sc.nextLine();
-
-
-        System.out.println("Enter user phone:");
+        System.out.println("Enter your  age");
+        String age = sc.nextLine();
+        System.out.println("Enter your phone");
         String phone= sc.nextLine();
+        System.out.println("Enter you Address");
+        String Address= sc.nextLine();
 
-        System.out.println("User details are:");
-        System.out.println("Name:- "+name);
+
+        System.out.println("Enter user details");
+        System.out.println("Name:- "+name );
         System.out.println("Age:- "+age);
-        System.out.println("Address:- "+address);
         System.out.println("Phone:- "+phone);
+        System.out.println("Address:- "+Address);
+
     }
 }
