eef8cbc069c9a980af1ebb2b26b365fa2edeb7c0
Satyaprakash <sk9334069077@gmail.com>
2025-05-12 03:20:35 +0530

    nested if_else  youngest  age among three ages

--- src/Conditional/Nested_if_else.java
+++ src/Conditional/Nested_if_else.java
@@ -21,32 +21,56 @@ public class Nested_if_else {
 //        }
 
 
-        System.out.println("Enter first number:");
-        int a= sc.nextInt();
-        System.out.println("Enter second number");
-        int b= sc.nextInt();
-        System.out.println("Enter third number:");
-        int c= sc.nextInt();
+//        System.out.println("Enter first number:");
+//        int a= sc.nextInt();
+//        System.out.println("Enter second number");
+//        int b= sc.nextInt();
+//        System.out.println("Enter third number:");
+//        int c= sc.nextInt();
+//
+//        if (a>b){
+//            if (a>c){
+//                System.out.println(a+ " is largest");
+//            }
+//            else {
+//                System.out.println(c + "  is largest");
+//            }
+//
+//        }
+//        else {
+//            if (b>c){
+//                System.out.println(b + "is largest");
+//            }
+//            else {
+//                System.out.println(c + " is largest");
+//        }
+ //
+//        }
 
-        if (a>b){
-            if (a>c){
-                System.out.println(a+ " is largest");
-            }
-            else {
-                System.out.println(c + "  is largest");
-            }
 
+
+
+        System.out.println("Enter  First Age:");
+        int aman= sc.nextInt();
+        System.out.println("Enter Second Age:");
+        int satya= sc.nextInt();
+        System.out.println("Enter Third Age:");
+        int rahul= sc.nextInt();
+        if (aman>satya){
+           if (aman>rahul){
+               System.out.println(aman+ " aman is youngest");
+           }
+           else {
+               System.out.println(rahul+ " rahul is youngest");
+           }
+        } else if (satya>rahul) {
+            System.out.println(satya+ "satya is youngest");
         }
-        else {
-            if (b>c){
-                System.out.println(b + "is largest");
-            }
-            else {
-                System.out.println(c + " is largest");
+else {
+            System.out.println(rahul+ "rahul is youngest");
         }
-
-
-        }
-
     }
+
+
+
 }
