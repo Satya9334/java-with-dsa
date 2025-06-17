65b1f28b95bee3085262395379ac4a912849cdfd
Satyaprakash <sk9334069077@gmail.com>
2025-06-17 22:13:15 +0530

    do_while loop using  number generate

---
+++ src/Loops/do_while.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class do_while {
+    public static void main(String[] args) {
+
+        int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+        } while (num<=20);
+
+
+    }
+}
